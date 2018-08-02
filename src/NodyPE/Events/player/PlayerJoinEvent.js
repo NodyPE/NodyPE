94f23d6a1b29c3a7280cc272f23ebfaee70c5fbf
ZeSp <16628342+ZeSpB@users.noreply.github.com>
2018-08-02 03:26:55 +0100

    Add packets

--- src/NodyPE/Events/player/PlayerJoinEvent.js
+++ src/NodyPE/Events/player/PlayerJoinEvent.js
@@ -1,3 +1,3 @@
 module.exports = async (client, Logger) => {
-    Logger.info(client.name + " joined! [" + client.address + "]")
+    Logger.info(client.name + " joined! [" + client.address + ":" + Client.port + "]")
 }
